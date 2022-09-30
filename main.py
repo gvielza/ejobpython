bded8d8079eedcbdb0291ea73842cfc026937e94
gvielza <80008338+gvielza@users.noreply.github.com>
2022-09-29 22:27:04 -0300

    ej2

--- main.py
+++ main.py
@@ -10,3 +10,20 @@ class Coche(Vehiculo):
 c=Coche
 c.Color="Verde"
 print("El color del coche ahora es ",c.Color)
+
+class Alumno:
+
+    def __int__(self):
+        self.nombre=input("ingrese su nombre: ")
+        self.nota=int(input("Ingrese su nota: "))
+
+    def isAprobado(self):
+        if(self.nota<60):
+            print(self.nombre, "estás reprobado☺ Ups!")
+        else:
+            print(self.nombre,"bieeen, aprobaste☻")
+
+alumno=Alumno()
+alumno.__int__()
+alumno.isAprobado()
+
