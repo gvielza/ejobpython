581f96efe46c815c9346c2752cc2d689b09740f3
gvielza <80008338+gvielza@users.noreply.github.com>
2022-10-15 13:23:37 -0300

    Ejercicio sqlite3

--- insertandodatossqlite.py
+++ insertandodatossqlite.py
@@ -4,14 +4,40 @@ import _sqlite3
 # que también será de tipo texto.Una vez creada la tabla, tenéis que insertarle datos,
 # como mínimo tenéis que insertar 8 alumnos a la tabla.
 # Por último, tienes que realizar una búsqueda de un alumno por nombre y mostrar los datos por consola.
+opciones=input("Para agregar un nuevo alumno presione 1 o 2 para ver lo que ya están insertados: ")
 
-conn=_sqlite3.connect("alumnos.db")
 
-cursor=conn.cursor()
-query='''INSERT INTO users(id,username,password) VALUES(?,?,?)'''
 
-rows=cursor.execute(query)
-conn.commit()
+def insertar_alumno(id,nombre,apellido):
+    conn=_sqlite3.connect("alumnos.db")
+    cursor=conn.cursor()
+    query='''INSERT INTO alumnos(id,nombre,apellido) VALUES(?,?,?)'''
+    rows=cursor.execute(query,(id,nombre,apellido))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    print('El alumno con id: ',id,", nombre: ",nombre,"y apellido: ",apellido," fue insertado en la BD")
+    mostrar()
 
-cursor.close()
-conn.close()
+def mostrar():
+    conn = _sqlite3.connect("alumnos.db")
+    cursor = conn.cursor()
+    query = '''SELECT * FROM alumnos'''
+    rows = cursor.execute(query)
+    for row in rows:
+        print(row)
+    cursor.close()
+    conn.close()
+
+
+
+def insertar():
+    id = input("Para Insertar un nuevo alumno escribe el identificador: ")
+    nombre = input("Ahora el nombre: ")
+    apellido = input("ahora el apellido: ")
+    insertar_alumno(id, nombre, apellido)
+
+if(int(opciones)==1):
+    insertar()
+else:
+    mostrar()
